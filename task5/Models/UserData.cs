89277c4eddd30e63f3d9b5546ed8ff4210eda751
Danila <154272273+DanilaMax2608@users.noreply.github.com>
2024-09-21 17:07:53 +0300

    Add files via upload

---
+++ task5/Models/UserData.cs
@@ -0,0 +1,11 @@
+﻿namespace task55.Models
+{
+    public class UserData
+    {
+        public int Id { get; set; }
+        public int Number { get; set; }
+        public string FullName { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
